4caf0f92132ee7670e2c9bb2b9a52b9e0e79a787
DhyeyLukhi <dhyeylukhi95@gmail.com>
2025-05-17 18:01:31 +0530

    just change the BE to CS for sister

--- Experiments/myfile.py
+++ Experiments/myfile.py
@@ -4,4 +4,4 @@ years = ['S', 'W']
 
 for time in years:
     for year in range(2021, 2025):
-        web.open(f"https://gtu.ac.in/uploads/{time}{year}/BE/3140709.pdf")
+        web.open(f"https://gtu.ac.in/uploads/{time}{year}/CS/1310503.pdf")
